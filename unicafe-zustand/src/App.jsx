59097eb3c9c28ce8c5b5a2d45654159ad4b1b620
Student <student@sirafrica.org>
2026-07-16 13:05:13 +0300

    6.6

--- unicafe-zustand/src/App.jsx
+++ unicafe-zustand/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import Filter from './components/Filter'
 
 const App = () => {
+  const [filter, setFilter] = useState('')
+
   return (
     <div>
+      <Filter onChange={setFilter} />
       <h2>Anecdotes</h2>
-      <AnecdoteList />
+      <AnecdoteList filter={filter} />
       <AnecdoteForm />
     </div>
   )
--- unicafe-zustand/src/components/AnecdoteList.jsx
+++ unicafe-zustand/src/components/AnecdoteList.jsx
@@ -1,10 +1,14 @@
 import useAnecdoteStore from '../store'
 
-const AnecdoteList = () => {
+const AnecdoteList = ({ filter }) => {
   const anecdotes = useAnecdoteStore((state) => state.anecdotes)
   const vote = useAnecdoteStore((state) => state.vote)
 
-  const sortedAnecdotes = anecdotes.toSorted((a, b) => b.votes - a.votes)
+  const filteredAnecdotes = anecdotes.filter((anecdote) =>
+    anecdote.content.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const sortedAnecdotes = filteredAnecdotes.toSorted((a, b) => b.votes - a.votes)
 
   return (
     <div>
