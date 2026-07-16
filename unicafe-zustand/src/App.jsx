c3aeeada5d5b8aef1fdfb712d462be119cda78da
Student <student@sirafrica.org>
2026-07-16 12:41:11 +0300

    6.2

--- unicafe-zustand/src/App.jsx
+++ unicafe-zustand/src/App.jsx
@@ -1,60 +1,21 @@
-import useFeedbackStore from './store'
-
-const StatisticLine = ({ text, value }) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-)
-
-const Statistics = () => {
-  const good = useFeedbackStore((state) => state.good)
-  const neutral = useFeedbackStore((state) => state.neutral)
-  const bad = useFeedbackStore((state) => state.bad)
-
-  const all = good + neutral + bad
-  const average = all === 0 ? 0 : (good - bad) / all
-  const positive = all === 0 ? 0 : (good / all) * 100
-
-  if (all === 0) {
-    return (
-      <div>
-        <h2>statistics</h2>
-        <p>No feedback given</p>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h2>statistics</h2>
-      <table>
-        <tbody>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={all} />
-          <StatisticLine text="average" value={average} />
-          <StatisticLine text="positive" value={`${positive} %`} />
-        </tbody>
-      </table>
-    </div>
-  )
-}
+import useAnecdoteStore from './store'
 
 const App = () => {
-  const goodClick = useFeedbackStore((state) => state.goodClick)
-  const neutralClick = useFeedbackStore((state) => state.neutralClick)
-  const badClick = useFeedbackStore((state) => state.badClick)
+  const anecdotes = useAnecdoteStore((state) => state.anecdotes)
+  const vote = useAnecdoteStore((state) => state.vote)
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={goodClick}>good</button>
-      <button onClick={neutralClick}>neutral</button>
-      <button onClick={badClick}>bad</button>
-
-      <Statistics />
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
--- unicafe-zustand/src/store.js
+++ unicafe-zustand/src/store.js
@@ -1,12 +1,34 @@
 import { create } from 'zustand'
 
-const useFeedbackStore = create((set) => ({
-  good: 0,
-  neutral: 0,
-  bad: 0,
-  goodClick: () => set((state) => ({ good: state.good + 1 })),
-  neutralClick: () => set((state) => ({ neutral: state.neutral + 1 })),
-  badClick: () => set((state) => ({ bad: state.bad + 1 })),
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Adding manpower to a late software project makes it later!',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => ({
+  content: anecdote,
+  id: getId(),
+  votes: 0,
+})
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const useAnecdoteStore = create((set) => ({
+  anecdotes: initialState,
+  vote: (id) =>
+    set((state) => ({
+      anecdotes: state.anecdotes.map((anecdote) =>
+        anecdote.id === id
+          ? { ...anecdote, votes: anecdote.votes + 1 }
+          : anecdote
+      ),
+    })),
 }))
 
-export default useFeedbackStore
+export default useAnecdoteStore
